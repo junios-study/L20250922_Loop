28ca1aba4b826f9a7d081520a8943750c0d8ba74
junios-study <junios@naver.com>
2025-09-22 14:28:49 +0900

    함수 선언으로 변경

--- main.cpp
+++ main.cpp
@@ -1,91 +1,97 @@
 #include <iostream> //cpp standard
 #include <conio.h> //c standard
-#include <vector>
-#include <algorithm>
-
 
 using namespace std;
 
-//**********
-//*        *
-//*        *
-//*        *
-//*        *
-//*        *
-//*        *
-//*        *
-//*        * 
-//**********
+int World[100][100] =
+{
+	{1, 1, 1, 1, 1, 1, 1, 1, 1, 1},
+	{1, 0, 0, 0, 0, 0, 0, 0, 0, 1},
+	{1, 0, 0, 0, 0, 0, 0, 0, 0, 1},
+	{1, 0, 0, 0, 0, 0, 0, 0, 0, 1},
+	{1, 0, 0, 0, 0, 0, 0, 0, 0, 1},
+	{1, 0, 0, 0, 0, 0, 0, 0, 0, 1},
+	{1, 0, 0, 0, 0, 0, 0, 0, 0, 1},
+	{1, 0, 0, 0, 0, 0, 0, 0, 0, 1},
+	{1, 0, 0, 0, 0, 0, 0, 0, 0, 1},
+	{1, 1, 1, 1, 1, 1, 1, 1, 1, 1}
+};
+
+bool bIsRunning = true;
+
+int PlayerX = 1;
+int PlayerY = 1;
+char PlayerShape = 'P';
+
+//사상(Mapping)
+char Sprites[10] = { ' ', '*', }; //그림으로 바꾸자.
+
+int KeyCode;
+
+void Input()
+{
+	//Input
+	KeyCode = _getch();
+}
+
+void Process()
+{
+	//Process
+	if (KeyCode == 'w')
+	{
+		PlayerY--;
+	}
+	else if (KeyCode == 's')
+	{
+		PlayerY++;
+	}
+	else if (KeyCode == 'a')
+	{
+		PlayerX--;
+	}
+	else if (KeyCode == 'd')
+	{
+		PlayerX++;
+	}
+	else if (KeyCode == 'q')
+	{
+		bIsRunning = false;
+	}
+}
+
+void Render()
+{
+	//Render();
+	system("cls");
+	for (int Y = 0; Y < 10; ++Y)
+	{
+		for (int X = 0; X < 10; ++X)
+		{
+			if (PlayerX == X && PlayerY == Y)
+			{
+				cout << PlayerShape;
+			}
+			else //other
+			{
+				cout << Sprites[World[Y][X]];
+			}
+
+		}
+		cout << endl;
+	}
+}
+
+
 int main()
 {
-	int World[100][100] =
-	{
-		{1, 1, 1, 1, 1, 1, 1, 1, 1, 1},
-		{1, 0, 0, 0, 0, 0, 0, 0, 0, 1},
-		{1, 0, 0, 0, 0, 0, 0, 0, 0, 1},
-		{1, 0, 0, 0, 0, 0, 0, 0, 0, 1},
-		{1, 0, 0, 0, 0, 0, 0, 0, 0, 1},
-		{1, 0, 0, 0, 0, 0, 0, 0, 0, 1},
-		{1, 0, 0, 0, 0, 0, 0, 0, 0, 1},
-		{1, 0, 0, 0, 0, 0, 0, 0, 0, 1},
-		{1, 0, 0, 0, 0, 0, 0, 0, 0, 1},
-		{1, 1, 1, 1, 1, 1, 1, 1, 1, 1}
-	};
 
-	bool bIsRunning = true;
 
-	int PlayerX = 1;
-	int PlayerY = 1;
-	char PlayerShape = 'P';
-
-	//사상(Mapping)
-	char Sprites[10] = { ' ', '*', }; //그림으로 바꾸자.
-
+	//frame, deltaseconds
 	while (bIsRunning)
 	{
-		//Input
-		int KeyCode = _getch();
-
-		//Process
-		if (KeyCode == 'w')
-		{
-			PlayerY--;
-		}
-		else if (KeyCode == 's')
-		{
-			PlayerY++;
-		}
-		else if (KeyCode == 'a')
-		{
-			PlayerX--;
-		}
-		else if (KeyCode == 'd')
-		{
-			PlayerX++;
-		}
-		else if (KeyCode == 'q')
-		{
-			bIsRunning = false;
-		}
-
-		//Render();
-		system("cls");
-		for (int Y = 0; Y < 10; ++Y)
-		{
-			for (int X = 0; X < 10; ++X)
-			{
-				if (PlayerX == X && PlayerY == Y)
-				{
-					cout << PlayerShape;
-				}
-				else //other
-				{
-					cout << Sprites[World[Y][X]];
-				}
-
-			}
-			cout << endl;
-		}
+		Input();
+		Process();
+		Render();
 	}
 
 	return 0;
