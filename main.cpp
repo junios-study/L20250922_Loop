390d9e0996d8e4af2b29f973c43fd9855399f585
junios-study <junios@naver.com>
2025-09-22 11:23:24 +0900

    반대편 삼각형
    반대편 삼각형

--- main.cpp
+++ main.cpp
@@ -16,14 +16,21 @@ int main()
 
 	for (int i = 1; i <= Count; ++i)
 	{
-		for (int j = 1; j < i; ++j)
-		{
-			cout << " ";
-		}
+		//for (int j = 1; j < i; ++j)
+		//{
+		//	cout << " ";
+		//}
 
-		for (int j = 1; j <= Count - (i - 1); ++j)
+		for (int j = 1; j <= Count; ++j)
 		{
-			cout << "*";
+			if (i > j)
+			{
+				cout << " ";
+			}
+			else
+			{
+				cout << "*";
+			}
 		}
 		cout << "\n";
 	}
