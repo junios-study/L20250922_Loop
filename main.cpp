62d6df1f869f18b79fe8368e64e389cff26c00d2
junios-study <junios@naver.com>
2025-09-22 14:10:48 +0900

    Player 움직이기
    Player 움직이기

--- main.cpp
+++ main.cpp
@@ -1,4 +1,8 @@
-#include <iostream>
+#include <iostream> //cpp standard
+#include <conio.h> //c standard
+#include <vector>
+#include <algorithm>
+
 
 using namespace std;
 
@@ -14,7 +18,7 @@ using namespace std;
 //**********
 int main()
 {
-	int World[10][10] =
+	int World[100][100] =
 	{
 		{1, 1, 1, 1, 1, 1, 1, 1, 1, 1},
 		{1, 0, 0, 0, 0, 0, 0, 0, 0, 1},
@@ -28,30 +32,60 @@ int main()
 		{1, 1, 1, 1, 1, 1, 1, 1, 1, 1}
 	};
 
+	bool bIsRunning = true;
+
 	int PlayerX = 1;
 	int PlayerY = 1;
 	char PlayerShape = 'P';
 
-	char Sprites[10] = { ' ', '*', };
+	//사상(Mapping)
+	char Sprites[10] = { ' ', '*', }; //그림으로 바꾸자.
 
-
-
-	//Render();
-	for (int Y = 0; Y < 10; ++Y)
+	while (bIsRunning)
 	{
-		for (int X = 0; X < 10; ++X)
-		{
-			if (PlayerX == X && PlayerY == Y)
-			{
-				cout << PlayerShape;
-			}
-			else //other
-			{
-				cout << Sprites[World[Y][X]];
-			}
+		//Input
+		int KeyCode = _getch();
 
+		//Process
+		if (KeyCode == 'w')
+		{
+			PlayerY--;
 		}
-		cout << endl;
+		else if (KeyCode == 's')
+		{
+			PlayerY++;
+		}
+		else if (KeyCode == 'a')
+		{
+			PlayerX--;
+		}
+		else if (KeyCode == 'd')
+		{
+			PlayerX++;
+		}
+		else if (KeyCode == 'q')
+		{
+			bIsRunning = false;
+		}
+
+		//Render();
+		system("cls");
+		for (int Y = 0; Y < 10; ++Y)
+		{
+			for (int X = 0; X < 10; ++X)
+			{
+				if (PlayerX == X && PlayerY == Y)
+				{
+					cout << PlayerShape;
+				}
+				else //other
+				{
+					cout << Sprites[World[Y][X]];
+				}
+
+			}
+			cout << endl;
+		}
 	}
 
 	return 0;
